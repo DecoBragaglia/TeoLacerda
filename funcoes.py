0a7e833434831f3de643e68e3ffa9c03e62baa4e
TeoLacerda <laacerdateo@gmail.com>
2025-04-29 16:32:16 -0300

    Question 5: Calcula pontos na regra avançada - Soma

--- funcoes.py
+++ funcoes.py
@@ -25,3 +25,9 @@ def calcula_pontos_regra_simples(dados):
         if 1 <= dado <= 6:
             pontos[dado] += dado
     return pontos
+
+def calcula_pontos_soma(dados):
+    total = 0 
+    for dado in dados:
+        total += dado
+    return total
